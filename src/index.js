f4f84c8d53364d1d5a5db0cc7f47fd95839f25a2
EvilaMany <lineman98@yandex.ru>
2018-09-13 07:37:14 +0300

    solution of eighth taskgit add .!

--- src/index.js
+++ src/index.js
@@ -1,3 +1,61 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+
+  var maxDiv = 0
+  var dividers = []
+   //ИЩЕМ МНОЖИТЕЛИ
+  while(base % 2 == 0)
+  {
+  	dividers[dividers.length] = 2
+  	base /= 2;
+  }
+
+  for(var div = 3;div <= Math.sqrt(base);div+=2)
+  {
+  	while(base % div == 0)
+  	{
+  		dividers[dividers.length] = div
+  		base /= div
+  	}
+  }
+  if(base != 1)
+  {
+ 	dividers[dividers.length] = base
+  }
+
+  //нходим множитель дающий наибольшее произведение
+  var dividersPowers = []
+
+  for(var div of dividers)
+  {
+  	if(dividersPowers[div] != undefined)
+  		dividersPowers[div]++;
+  	else
+  		dividersPowers[div] = 1
+  } 
+
+  //console.log(dividersPowers)
+  //console.log(dividers)
+
+
+  var minZeros = 0;
+  for(var thisDiv in dividersPowers)
+  {
+  	if(typeof(thisDiv) == undefined)
+  		continue;
+
+  	var count = 0
+  	for(div = thisDiv; div <= number;div*=thisDiv)
+	{
+	  count += Math.floor(number / div)
+	}
+	count /= dividersPowers[thisDiv]
+	
+
+	if(minZeros == 0 || count < minZeros)
+		minZeros = count
+  }
+
+  return Math.floor(minZeros)
 }
+
+//console.log(getZerosCount(88341643, 66))
